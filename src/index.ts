93801d736bab72c654af0fbfd80cc863450e62a6
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-05-20 10:56:09 -0700

    export SideMenu

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import "resize-observer-polyfill";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import BoardFeed from "./board/BoardFeed";
-// import SideMenu from "./SideMenu";
+import SideMenu from "./SideMenu";
 import Button from "./common/Button";
 import Layout from "./Layout";
 
-export { Layout, BoardFeed, Button };
+export { Layout, BoardFeed, Button, SideMenu };
