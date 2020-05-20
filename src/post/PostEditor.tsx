71a3d01c471fc914be294dbccac23c96ec07e291
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-05-20 15:56:05 -0700

    create loading style for post editor

--- src/post/PostEditor.tsx
+++ src/post/PostEditor.tsx
@@ -3,19 +3,23 @@ import React from "react";
 import Header, { HeaderStyle } from "./Header";
 import Footer, { modes as footerModes } from "./Footer";
 import Card, { CardSizes } from "../common/Card";
+import Spinner from "../common/Spinner";
 import Editor from "@bobaboard/boba-editor";
 
 import Theme from "../theme/default";
 import Button from "../common/Button";
 import { faPortrait, faImage } from "@fortawesome/free-solid-svg-icons";
+import classnames from "classnames";
 
 const PostEditor: React.FC<PostEditorProps> = (props) => {
   const [size, setNewSize] = React.useState(
     props.defaultSize || CardSizes.REGULAR
   );
   const [newText, setNewText] = React.useState(
-    props.initialText ? JSON.parse(props.initialText) : ""
+    props.initialText ? props.initialText : ""
   );
+  const [isEmpty, setIsEmpty] = React.useState(true);
+
   return (
     <>
       <div className="post-container">
@@ -36,6 +40,7 @@ const PostEditor: React.FC<PostEditorProps> = (props) => {
                         : CardSizes.REGULAR
                     )
                   }
+                  disabled={props.loading}
                 >
                   {size == CardSizes.REGULAR ? "Enlarge" : "Shrink"}
                 </Button>
@@ -46,23 +51,47 @@ const PostEditor: React.FC<PostEditorProps> = (props) => {
             <div className="footer">
               <Footer
                 mode={footerModes.CREATE}
-                onSubmit={() => props.onSubmit(newText)}
+                onSubmit={() =>
+                  props.onSubmit({
+                    text: newText,
+                    large: size == CardSizes.WIDE,
+                  })
+                }
                 onCancel={props.onCancel}
+                submittable={!props.loading && !isEmpty}
+                cancellable={!props.loading}
               />
             </div>
           }
           size={size}
         >
-          <div className="editor">
-            <Editor
-              initialText={
-                props.initialText ? JSON.parse(props.initialText) : ""
-              }
-              editable={true}
-              focus={true}
-              onSubmit={() => props.onSubmit(newText)}
-              onTextChange={(text) => setNewText(JSON.stringify(text.ops))}
-            />
+          <div
+            className={classnames("editor-container", {
+              loading: props.loading,
+            })}
+          >
+            <div className={"spinner"}>
+              <Spinner />
+            </div>
+            <div className="editor">
+              <Editor
+                initialText={
+                  props.initialText ? JSON.parse(props.initialText) : ""
+                }
+                editable={!props.loading}
+                focus={true}
+                onSubmit={() =>
+                  props.onSubmit({
+                    text: newText,
+                    large: size == CardSizes.WIDE,
+                  })
+                }
+                onIsEmptyChange={(empty) => {
+                  setIsEmpty(empty);
+                }}
+                onTextChange={(text) => setNewText(JSON.stringify(text.ops))}
+              />
+            </div>
           </div>
         </Card>
       </div>
@@ -83,9 +112,27 @@ const PostEditor: React.FC<PostEditorProps> = (props) => {
           background-color: ${Theme.POST_BACKGROUND_COLOR};
           padding: 15px;
         }
-        .editor {
+        .editor-container {
           padding-top: 5px;
           min-height: 300px;
+        }
+        .editor-container.loading .editor {
+          opacity: 0.5;
+        }
+        .spinner {
+          position: absolute;
+          top: 0;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          justify-content: center;
+          align-items: center;
+          z-index: 100;
+          display: none;
+        }
+
+        .editor-container.loading .spinner {
+          display: flex;
         }
       `}</style>
     </>
@@ -104,7 +151,8 @@ export interface PostEditorProps {
     avatar: string;
     name: string;
   };
+  loading?: boolean;
   defaultSize?: CardSizes;
-  onSubmit: (text: string) => void;
+  onSubmit: (post: { text: string; large: boolean }) => void;
   onCancel: () => void;
 }
