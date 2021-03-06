9507fbe957e4004675aba9416dbf0820eec1f067
Ben Silverman <bsilverm@mit.edu>
2021-03-06 17:52:28 -0500

    Replace :glboal with css.resolve

--- src/common/SegmentedButton.tsx
+++ src/common/SegmentedButton.tsx
@@ -5,12 +5,52 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCertificate } from "@fortawesome/free-solid-svg-icons";
 import Color from "color";
 import classnames from "classnames";
+import css from "styled-jsx/css";
+
+const getLinkStyle = (color: string, reverseColor: string) => css.resolve`
+  .option-link {
+    border-radius: 25px;
+    padding: 8px 12px;
+    background-image: none;
+    color: ${color};
+    background-color: ${reverseColor};
+    border: 2px solid ${reverseColor};
+    transition: all 0.2s linear 0s;
+    box-sizing: border-box;
+    display: inline-block;
+    width: 100%;
+  }
+  .option-link:focus {
+    outline: none;
+  }
+  .option-link:hover {
+    cursor: pointer;
+    background-color: ${Color(color).alpha(0.5)};
+  }
+`;
+
+const getSelectedLinkStyle = (color: string, reverseColor: string) => css.resolve`
+  .option-link {
+    border: 2px solid ${reverseColor};
+    color: ${reverseColor};
+    background-color: ${color};
+  }
+  .option-link:hover {
+    background-color: ${color};
+  }
+`;
 
 const SegmentedButton: React.FC<SegmentedButtonProps> = (props) => {
   const [selected, setSelected] = useState(props.selected);
   const THEME_COLOR = getThemeColor(props.theme);
   const REVERSE_THEME_COLOR = getReverseThemeColor(props.theme);
   const transparent = ButtonStyle.TRANSPARENT == props.theme;
+  const { className: linkClass, styles: linkStyles } = getLinkStyle(
+    props.color || THEME_COLOR, REVERSE_THEME_COLOR
+  );
+  const { className: selectedLinkClass, styles: selectedLinkStyles } = getSelectedLinkStyle(
+    props.color || THEME_COLOR, REVERSE_THEME_COLOR
+  );
   return (
     <div className={classnames("segmented-button", {})}>
       {props.options.map((option) => (
@@ -42,10 +82,12 @@ const SegmentedButton: React.FC<SegmentedButtonProps> = (props) => {
               },
               href: option.href,
             }}
-            className="segmented-button-link"
+            className={`option-link ${linkClass} ${selected == option.id ? selectedLinkClass : ''}`}
             allowDefault={!!option.href && !option.onClick}
           >
             {option.label}
+            {linkStyles}
+            {selected == option.id && selectedLinkStyles}
           </ActionLink>
         </div>
       ))}
@@ -61,38 +103,6 @@ const SegmentedButton: React.FC<SegmentedButtonProps> = (props) => {
           text-align: center;
           position: relative;
           flex: 1;
-        }
-        .segmented-button-option > :global(button),
-        .segmented-button-option > :global(a) {
-          border-radius: 25px;
-          padding: 8px 12px;
-          background-image: none;
-          color: ${props.color || THEME_COLOR};
-          background-color: ${REVERSE_THEME_COLOR};
-          border: 2px solid ${REVERSE_THEME_COLOR};
-          transition: all 0.2s linear 0s;
-          box-sizing: border-box;
-          display: inline-block;
-          width: 100%;
-        }
-        .segmented-button-option > :global(button):focus,
-        .segmented-button-option > :global(a):focus {
-          outline: none;
-        }
-        .segmented-button-option:not(.selected):hover > :global(button),
-        .segmented-button-option:not(.selected):hover > :global(a) {
-          background-color: ${Color(props.color || THEME_COLOR).alpha(0.5)};
-          cursor: pointer;
-        }
-        .segmented-button-option.selected:hover > :global(button),
-        .segmented-button-option.selected:hover > :global(a) {
-          cursor: pointer;
-        }
-        .selected > :global(button),
-        .selected > :global(a) {
-          border: 2px solid ${REVERSE_THEME_COLOR};
-          color: ${REVERSE_THEME_COLOR};
-          background-color: ${props.color || THEME_COLOR};
         }
         .updates {
           background-color: ${props.color || THEME_COLOR};
