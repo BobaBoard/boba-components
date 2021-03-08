c584cdf185cdc26361ddbc757e54bb4fdd2b15f7
Ben Silverman <bsilverm@mit.edu>
2021-03-07 19:42:57 -0500

    Pass an object with arg keys to getOptionStyle

--- src/common/SegmentedButton.tsx
+++ src/common/SegmentedButton.tsx
@@ -8,7 +8,7 @@ import Color from "color";
 import classnames from "classnames";
 import css from "styled-jsx/css";
 
-const getOptionsStyle = (color: string, reverseColor: string, transparent: boolean) => css.resolve`
+const getOptionsStyle = (settings: {color: string, reverseColor: string, transparent: boolean}) => css.resolve`
   .option {
     display: inline-block;
     text-align: center;
@@ -16,9 +16,9 @@ const getOptionsStyle = (color: string, reverseColor: string, transparent: boole
     flex: 1;
   }
   .updates {
-    background-color: ${color};
-    border: 2px solid ${reverseColor};
-    color: ${transparent ? "black" : reverseColor};
+    background-color: ${settings.color};
+    border: 2px solid ${settings.reverseColor};
+    color: ${settings.transparent ? "black" : settings.reverseColor};
     position: absolute;
     border-radius: 50%;
     width: 20px;
@@ -36,9 +36,9 @@ const getOptionsStyle = (color: string, reverseColor: string, transparent: boole
     border-radius: 25px;
     padding: 8px 12px;
     background-image: none;
-    color: ${color};
-    background-color: ${reverseColor};
-    border: 2px solid ${reverseColor};
+    color: ${settings.color};
+    background-color: ${settings.reverseColor};
+    border: 2px solid ${settings.reverseColor};
     transition: all 0.2s linear 0s;
     box-sizing: border-box;
     display: inline-block;
@@ -49,24 +49,26 @@ const getOptionsStyle = (color: string, reverseColor: string, transparent: boole
   }
   .option-link:hover {
     cursor: pointer;
-    background-color: ${Color(color).alpha(0.5)};
+    background-color: ${Color(settings.color).alpha(0.5)};
   }
   .selected {
-    border: 2px solid ${reverseColor};
-    color: ${reverseColor};
-    background-color: ${color};
+    border: 2px solid ${settings.reverseColor};
+    color: ${settings.reverseColor};
+    background-color: ${settings.color};
   }
   .selected:hover {
-    background-color: ${color};
+    background-color: ${settings.color};
   }
 `;
 
 const SegmentedButtonOption: React.FC<SegmentedButtonOptionProps> = (props) => {
   const THEME_COLOR = getThemeColor(props.theme);
   const REVERSE_THEME_COLOR = getReverseThemeColor(props.theme);
-  const { className: optionsClass, styles: optionsStyles } = getOptionsStyle(
-    props.color || THEME_COLOR, REVERSE_THEME_COLOR, ButtonStyle.TRANSPARENT == props.theme
-  );
+  const { className: optionsClass, styles: optionsStyles } = getOptionsStyle({
+    color: props.color || THEME_COLOR,
+    reverseColor: REVERSE_THEME_COLOR,
+    transparent: ButtonStyle.TRANSPARENT == props.theme
+  });
   return (
     <div
       className={classnames(
