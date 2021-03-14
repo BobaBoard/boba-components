1ebf4d979d477bb7f9e7c2a34c74b940fa19724a
Essential Randomness <essential.randomn3ss@gmail.com>
2021-03-14 15:43:31 -0700

    do an in-house version of the FAB

--- src/board/CycleNewButton.tsx
+++ src/board/CycleNewButton.tsx
@@ -5,19 +5,24 @@ import { faCertificate } from "@fortawesome/free-solid-svg-icons";
 export interface CycleNewButtonProps {
   onNext: () => void;
   text: string;
+  loading?: boolean;
 }
 
 const CycleNewButton: React.FC<CycleNewButtonProps> = (props) => {
   return (
     <FloatingActionButton
+      spin={props.loading}
       accentColor={"grey"}
-      actions={[
-        {
-          icon: faCertificate,
-          tooltip: props.text,
-          action: props.onNext,
-        },
-      ]}
+      actions={React.useMemo(
+        () => [
+          {
+            icon: faCertificate,
+            tooltip: props.text,
+            action: props.onNext,
+          },
+        ],
+        [props.onNext, props.text]
+      )}
     />
   );
 };
--- src/common/FloatingActionButton.tsx
+++ src/common/FloatingActionButton.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
-import { useWindowWidth } from "@react-hook/window-size";
-import { Fab } from "@rmwc/fab";
-import "@rmwc/fab/styles";
-
 import Theme from "../theme/default";
+import classNames from "classnames";
 
 export interface FloatingActionButtonProps {
   accentColor?: string;
+  spin?: boolean;
   actions: {
     icon: IconDefinition;
     tooltip: string;
@@ -17,38 +15,43 @@ export interface FloatingActionButtonProps {
 }
 
 const FloatingActionButton: React.FC<FloatingActionButtonProps> = (props) => {
-  const windowWidth = useWindowWidth({ initialWidth: 0 });
-  const [mini, setMini] = React.useState(false);
-  if (typeof window === "undefined") {
-    return <div />;
-  }
-
-  React.useEffect(() => {
-    setMini(windowWidth < 460);
-  }, [windowWidth]);
-
-  // TODO: re-enable multiple actions
-  // See: https://www.npmjs.com/package/rmwc-fabmenu
   return (
-    <div className="fab-container">
-      <Fab
-        onClick={props.actions[0].action}
-        mini={mini}
-        style={{
-          backgroundColor: props.accentColor || Theme.DEFAULT_ACCENT_COLOR,
-        }}
-      >
+    <div className={classNames("fab-container")}>
+      <button className="fab" onClick={props.actions[0].action}>
         <div className="icon">
-          <FontAwesomeIcon icon={props.actions[0].icon} />
+          <FontAwesomeIcon icon={props.actions[0].icon} spin={props.spin} />
         </div>
-      </Fab>
+      </button>
       <style jsx>{`
         .fab-container {
           position: fixed;
           bottom: 2rem;
           right: 2rem;
-          transition: right 0.3s ease-out;
           z-index: 6;
+        }
+        .fab {
+          border: 0;
+          margin: 0;
+          padding: 0;
+          background-color: ${props.accentColor || Theme.DEFAULT_ACCENT_COLOR};
+          color: white;
+          text-align: center;
+          position: fixed;
+          right: 20px;
+          bottom: 20px;
+          width: 56px;
+          height: 56px;
+          font-size: 16px;
+          border-radius: 50%;
+          transition: all 0.1s ease-in-out;
+          box-shadow: 0 4px 5px 0 rgb(0 0 0 / 14%),
+            0 1px 10px 0 rgb(0 0 0 / 12%), 0 2px 4px -1px rgb(0 0 0 / 20%);
+        }
+        .fab:hover {
+          box-shadow: 0 4px 5px 0 rgb(0 0 0 / 20%),
+            0 1px 10px 0 rgb(0 0 0 / 18%), 0 2px 4px -1px rgb(0 0 0 / 28%);
+          transform: scale(1.048);
+          cursor: pointer;
         }
         .icon {
           position: absolute;
@@ -56,9 +59,12 @@ const FloatingActionButton: React.FC<FloatingActionButtonProps> = (props) => {
           left: 50%;
           transform: translate(-50%, -50%);
         }
-
-        :global(.side-menu-open) .fab-container {
-          right: min(calc(-100vw + 100px - 2rem), calc(-500px + 2rem));
+        @media only screen and (max-width: 460px) {
+          .fab {
+            width: 35px;
+            height: 35px;
+            font-size: 13px;
+          }
         }
       `}</style>
     </div>
