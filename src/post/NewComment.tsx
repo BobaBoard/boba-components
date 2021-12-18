0514791960e4e4e0c6f28b92b7c438480be8557b
Essential Randomness <essential.randomn3ss@gmail.com>
2021-12-18 14:12:56 -0800

    create new comment class to swap out the old, ill-advised one

---
+++ src/post/NewComment.tsx
@@ -0,0 +1,279 @@
+import Header, { HeaderStyle } from "./Header";
+import {
+  faCertificate,
+  faComment,
+  faCrown,
+} from "@fortawesome/free-solid-svg-icons";
+
+import Badge from "./Badge";
+import { DropdownProps } from "../common/DropdownListMenu";
+import Editor from "@bobaboard/boba-editor";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { SecretIdentityType } from "types";
+import Theme from "../theme/default";
+import classnames from "classnames";
+import debug from "debug";
+
+const log = debug("bobaui:comment-log");
+
+const Comment = React.forwardRef<CommentHandler, CommentProps>(
+  (
+    {
+      comments,
+      secretIdentity,
+      userIdentity,
+      options,
+      createdTime,
+      forceHideIdentity,
+      disableMotionOnScroll,
+      op,
+      new: isNew,
+      onExtraAction,
+    },
+    ref
+  ) => {
+    const headerRef = React.useRef<HTMLDivElement>(null);
+    const avatarRef = React.useRef<HTMLDivElement>(null);
+    const commentContainerRef = React.useRef<HTMLDivElement>(null);
+
+    React.useImperativeHandle(ref, () => ({
+      highlight: (color: string) => {
+        console.log("highlight");
+        log(`Highlighting comment with ${color}!`);
+        if (!commentContainerRef.current) {
+          return;
+        }
+        commentContainerRef.current.ontransitionend = () => {
+          commentContainerRef.current?.style.setProperty(
+            "--comment-container-shadow",
+            null
+          );
+        };
+        commentContainerRef.current.style.setProperty(
+          "--comment-container-shadow",
+          color
+        );
+      },
+      avatarRef,
+    }));
+
+    return (
+      <div className="comment-chain-container">
+        <div className="header" ref={headerRef}>
+          <Header
+            size={HeaderStyle.COMPACT}
+            secretIdentity={secretIdentity}
+            userIdentity={userIdentity}
+            avatarOptions={options}
+            createdMessage={createdTime}
+            forceHide={forceHideIdentity}
+            ref={avatarRef}
+          />
+        </div>
+        <div className={classnames("badges")}>
+          {!!isNew && (
+            <div className="badge">
+              <Badge
+                icon={faCertificate}
+                label="new"
+                color={Theme.DEFAULT_ACCENT_COLOR}
+              />
+            </div>
+          )}
+          {!!op && (
+            <div className="badge">
+              <Badge label="OP" color={Theme.OP_BADGE_COLOR} icon={faCrown} />
+            </div>
+          )}
+        </div>
+        <div className="comments-container" ref={commentContainerRef}>
+          {comments.map((comment) => (
+            <div key={comment.id} className={classnames("comment")}>
+              <Editor
+                initialText={JSON.parse(comment.text)}
+                singleLine={true}
+                forceSSR={true}
+              />
+            </div>
+          ))}
+        </div>
+        <button
+          className={classnames("extra-action", {
+            visible: !!onExtraAction,
+          })}
+          onClick={onExtraAction}
+        >
+          <div className="extra-action-icon">
+            <FontAwesomeIcon icon={faComment} />
+          </div>
+        </button>
+        <style jsx>{`
+          .comment-chain-container {
+            position: relative;
+            align-items: start;
+            display: flex;
+            --comment-container-stacked-radius: 0;
+            max-width: ${Theme.POST_WIDTH_PX}px;
+            width: 100%;
+            margin-bottom: 14px;
+            --text-padding: 13px;
+          }
+          .header {
+            cursor: pointer;
+            position: ${disableMotionOnScroll ? "relative" : "sticky"};
+            top: ${disableMotionOnScroll
+              ? "0"
+              : `${Theme.HEADER_HEIGHT_PX + 2}px`};
+            margin-right: 3px;
+          }
+          .badges {
+            display: flex;
+            justify-content: flex-end;
+            padding-right: 18px;
+            position: absolute;
+            top: 1px;
+            left: 0;
+            right: 0;
+            z-index: 3;
+            transform: translateY(-50%);
+          }
+          .badges .badge + .badge {
+            margin-left: 5px;
+          }
+          .comment-chain {
+            position: relative;
+            align-items: start;
+            display: flex;
+            --comment-container-stacked-radius: 0;
+            max-width: ${Theme.POST_WIDTH_PX}px;
+            width: 100%;
+            margin-bottom: 14px;
+            --text-padding: 13px;
+          }
+          .extra-action {
+            position: absolute;
+            bottom: 3px;
+            right: 16px;
+            transform: translate(50%, 50%);
+            display: none;
+            background-color: transparent;
+            padding: 0;
+            border: 0;
+          }
+          .extra-action:focus {
+            outline: none;
+          }
+          .extra-action:focus-visible {
+            outline: auto;
+          }
+          .extra-action-icon {
+            border-radius: 50%;
+            background-color: ${Theme.COMMENT_BACKGROUND_COLOR};
+            border: 1px solid ${Theme.COMMENT_BORDER_COLOR};
+            width: 20px;
+            height: 20px;
+            margin: 15px;
+            margin-right: 0px;
+            position: relative;
+            transition: all 0.2s ease-out;
+          }
+          .extra-action-icon :global(svg) {
+            color: ${Theme.COMMENT_TEXT_COLOR};
+            opacity: 0.6;
+          }
+          .extra-action.visible {
+            display: block;
+          }
+          .extra-action :global(svg) {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%) scale(0.8);
+          }
+          .extra-action:hover {
+            cursor: pointer;
+          }
+          .extra-action:hover .extra-action-icon :global(svg) {
+            color: ${Theme.COMMENT_TEXT_COLOR};
+            background-color: ${Theme.COMMENT_BACKGROUND_COLOR};
+            opacity: 1;
+          }
+          .comments-container {
+            font-size: var(--font-size-regular);
+            position: relative;
+            max-width: ${Theme.POST_WIDTH_PX}px;
+            width: calc(100% - 3px);
+            box-shadow: 0px 0px 5px 3px var(--comment-container-shadow);
+            transition: box-shadow 0.5s ease-out;
+            border-radius: ${Theme.BORDER_RADIUS_REGULAR};
+          }
+          .comments-container::after {
+            content: "";
+            border: 2px solid ${Theme.COMMENT_BORDER_COLOR};
+            box-sizing: border-box;
+            top: 0px;
+            bottom: 0px;
+            left: 0px;
+            right: 0px;
+            position: absolute;
+            z-index: 0;
+            width: 100%;
+            height: 100%;
+            border-radius: ${Theme.BORDER_RADIUS_REGULAR};
+            pointer-events: none;
+          }
+          .comment {
+            position: relative;
+            padding: 0px;
+            min-width: 0;
+            align-self: flex-end;
+            --text-color: ${Theme.COMMENT_TEXT_COLOR};
+            background: ${Theme.COMMENT_BACKGROUND_COLOR};
+            min-height: 38px;
+          }
+          .comment:last-of-type,
+          .comment:last-of-type :global(img) {
+            border-bottom-left-radius: ${Theme.BORDER_RADIUS_REGULAR};
+            border-bottom-right-radius: ${Theme.BORDER_RADIUS_REGULAR};
+          }
+          .comment:first-of-type,
+          .comment:first-of-type :global(img) {
+            border-top-left-radius: ${Theme.BORDER_RADIUS_REGULAR};
+            border-top-right-radius: ${Theme.BORDER_RADIUS_REGULAR};
+          }
+          .comment:not(:first-of-type) {
+            border-top: 1px dashed ${Theme.COMMENT_BORDER_COLOR};
+          }
+        `}</style>
+      </div>
+    );
+  }
+);
+Comment.displayName = "Comment";
+
+export interface CommentHandler {
+  highlight: (color: string) => void;
+  editorRef?: React.RefObject<HTMLDivElement>;
+  avatarRef?: React.RefObject<HTMLDivElement>;
+  headerRef?: React.RefObject<HTMLDivElement>;
+}
+
+export interface CommentProps {
+  comments: { id: string; text: string }[];
+  secretIdentity: SecretIdentityType;
+  userIdentity?: {
+    avatar: string;
+    name: string;
+  };
+  forceHideIdentity?: boolean;
+  createdTime: string;
+  new?: boolean;
+  op?: boolean;
+  options?: DropdownProps["options"];
+  onExtraAction?: () => void;
+  disableMotionOnScroll?: boolean;
+  ref?: React.Ref<CommentHandler>;
+}
+
+export default Comment;
--- stories/06-Comments/01-Comment.stories.tsx
+++ stories/06-Comments/01-Comment.stories.tsx
@@ -1,20 +1,19 @@
-import CommentChain, { CommentChainProps } from "../../src/post/CommentChain";
-import {
-  EDITOR_TEXT_VALUES,
-  EditorControlsType,
-  editorArgTypes,
-  getInitialText,
-} from "../utils/editor-controls";
 import { Meta, Story } from "@storybook/react";
+import NewComment, {
+  CommentHandler,
+  CommentProps,
+} from "../../src/post/NewComment";
 
 import React from "react";
+import { StoryFnReactReturnType } from "@storybook/react/dist/ts3.9/client/preview/types";
 import { action } from "@storybook/addon-actions";
+import { editorArgTypes } from "../utils/editor-controls";
 import mamoruAvatar from "../images/mamoru.png";
 import tuxedoAvatar from "../images/tuxedo-mask.jpg";
 
 export default {
   title: "Comment/Comment",
-  component: CommentChain,
+  component: NewComment,
   argTypes: {
     ...editorArgTypes,
   },
@@ -35,11 +34,11 @@ export default {
   ],
 } as Meta;
 
-const CommentTemplate: Story<CommentChainProps> = (args) => {
-  return <CommentChain {...args} ref={undefined} />;
+const CommentTemplate: Story<CommentProps> = (args) => {
+  return <NewComment {...args} ref={args.ref || undefined} />;
 };
 
-const WRAP_COMPACT = (Story: any) => {
+const WRAP_COMPACT = (Story: () => StoryFnReactReturnType) => {
   return (
     <div className="story">
       {Story()}
@@ -68,6 +67,13 @@ export const CompactTextComment = CommentTemplate.bind({});
 CompactTextComment.args = TextComment.args;
 CompactTextComment.decorators = [WRAP_COMPACT];
 
+export const CompactTextWithAction = CommentTemplate.bind({});
+CompactTextWithAction.args = {
+  ...CompactTextComment.args,
+  onExtraAction: () => action("click")(),
+};
+CompactTextWithAction.decorators = [WRAP_COMPACT];
+
 export const ImageComment = CommentTemplate.bind({});
 ImageComment.args = {
   comments: [
@@ -79,18 +85,79 @@ ImageComment.args = {
   ],
   secretIdentity: { name: "Tuxedo Mask", avatar: `/${tuxedoAvatar}` },
   userIdentity: { name: "SexyDaddy69", avatar: `/${mamoruAvatar}` },
+  createdTime: "At some point.",
 };
 
 export const CompactImageComment = CommentTemplate.bind({});
 CompactImageComment.args = ImageComment.args;
 CompactImageComment.decorators = [WRAP_COMPACT];
 
-export const ImageWithAction = CommentTemplate.bind({});
-ImageWithAction.args = {
+export const CompactImageCommentWithAction = CommentTemplate.bind({});
+CompactImageCommentWithAction.args = {
   ...ImageComment.args,
   onExtraAction: () => action("click")(),
 };
+CompactImageCommentWithAction.decorators = [WRAP_COMPACT];
 
-export const CompactImageWithAction = CommentTemplate.bind({});
-CompactImageWithAction.args = ImageWithAction.args;
-CompactImageWithAction.decorators = [WRAP_COMPACT];
+export const ChainComment = CommentTemplate.bind({});
+ChainComment.args = {
+  comments: [
+    {
+      id: "689c6467-4dfd-4434-8bca-a8f1b04bc1e8",
+      text:
+        '[{"insert": "I\'m a man of simple taste. I see half an ankle and go:"}]',
+    },
+    {
+      id: "91648c92-d86b-4e31-86c8-5f0e3b7a04a7",
+      text:
+        '[{"insert":{"block-image":{"src":"https://media.tenor.com/images/2d4aeafd88c82922635b972e454c07d3/tenor.gif","spoilers":false,"width":320,"height":176}}},{"insert":""}]',
+    },
+  ],
+  secretIdentity: { name: "Tuxedo Mask", avatar: `/${tuxedoAvatar}` },
+  userIdentity: { name: "SexyDaddy69", avatar: `/${mamoruAvatar}` },
+  createdTime: "At some point.",
+};
+
+export const CompactChainComment = CommentTemplate.bind({});
+CompactChainComment.args = ChainComment.args;
+CompactChainComment.decorators = [WRAP_COMPACT];
+
+export const CompactChainCommentWithAction = CommentTemplate.bind({});
+CompactChainCommentWithAction.args = {
+  ...CompactChainComment.args,
+  onExtraAction: () => action("click")(),
+};
+CompactChainCommentWithAction.decorators = [WRAP_COMPACT];
+
+export const CompactChainCommentWithBadges = CommentTemplate.bind({});
+CompactChainCommentWithBadges.args = {
+  ...CompactChainCommentWithAction.args,
+  op: true,
+  new: true,
+};
+CompactChainCommentWithBadges.decorators = [WRAP_COMPACT];
+
+export const CompactChainCommentWithHighlight = CommentTemplate.bind({});
+CompactChainCommentWithHighlight.args = {
+  ...CompactChainCommentWithBadges.args,
+};
+CompactChainCommentWithHighlight.decorators = [
+  WRAP_COMPACT,
+  (Story, storyArgs) => {
+    const commentRef = React.createRef<CommentHandler>();
+    storyArgs.args.ref = commentRef;
+    return (
+      <div className="story">
+        {Story()}
+        <button onClick={() => commentRef.current?.highlight("red")}>
+          Highlight
+        </button>
+        <style jsx>{`
+          .story {
+            max-width: 250px;
+          }
+        `}</style>
+      </div>
+    );
+  },
+];
