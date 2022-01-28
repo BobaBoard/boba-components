2c4cfa8b1808784fd6c0b4ea04102d953497787c
Essential Randomness <essential.randomn3ss@gmail.com>
2022-01-28 01:00:11 -0800

    style toasts better

--- stories/9-Toasts.stories.tsx
+++ stories/9-Toasts.stories.tsx
@@ -1,6 +1,7 @@
-import React from "react";
 //import { linkTo } from "@storybook/addon-links";
 import ToastContainer, { toast } from "../src/common/Toast";
+
+import React from "react";
 
 export default {
   title: "Toast Preview",
@@ -8,26 +9,99 @@ export default {
 };
 
 export const SimpleButton = () => {
+  const [successId, setSuccessId] =
+    React.useState<string | number | undefined>(undefined);
+  const [errorId, setErrorId] =
+    React.useState<string | number | undefined>(undefined);
+  const [warningId, setWarningId] =
+    React.useState<string | number | undefined>(undefined);
   React.useEffect(() => {
-    toast.success("🦄 Wow so easy!", {
-      autoClose: false,
-      hideProgressBar: true,
-      closeOnClick: false,
-    });
-    toast.error("🦄 Wow so easy!", {
-      autoClose: false,
-      hideProgressBar: true,
-      closeOnClick: false,
-    });
-    toast.warning("🦄 Wow so easy!", {
-      autoClose: false,
-      hideProgressBar: true,
-      closeOnClick: false,
-    });
+    setSuccessId(
+      toast.success("🦄 Wow so easy!", {
+        autoClose: false,
+        hideProgressBar: true,
+        closeOnClick: false,
+      })
+    );
+    setWarningId(
+      toast.warning("🦄 Wow so easy!", {
+        autoClose: false,
+        hideProgressBar: true,
+        closeOnClick: false,
+      })
+    );
+    setErrorId(
+      toast.error("🦄 Wow so easy!", {
+        autoClose: false,
+        hideProgressBar: true,
+        closeOnClick: false,
+      })
+    );
   }, []);
   return (
     <div>
       <ToastContainer />
+      <div
+        style={{
+          position: "fixed",
+          bottom: "50px",
+        }}
+      >
+        <button
+          onClick={() => {
+            if (successId) {
+              toast.dismiss(successId);
+              setSuccessId(undefined);
+            } else {
+              setSuccessId(
+                toast.success("🦄 Wow so easy!", {
+                  autoClose: false,
+                  hideProgressBar: true,
+                  closeOnClick: false,
+                })
+              );
+            }
+          }}
+        >
+          Toggle success
+        </button>
+        <button
+          onClick={() => {
+            if (warningId) {
+              toast.dismiss(warningId);
+              setWarningId(undefined);
+            } else {
+              setWarningId(
+                toast.warning("🦄 Wow so easy!", {
+                  autoClose: false,
+                  hideProgressBar: true,
+                  closeOnClick: false,
+                })
+              );
+            }
+          }}
+        >
+          Toggle warning
+        </button>
+        <button
+          onClick={() => {
+            if (errorId) {
+              toast.dismiss(errorId);
+              setErrorId(undefined);
+            } else {
+              setErrorId(
+                toast.error("🦄 Wow so easy!", {
+                  autoClose: false,
+                  hideProgressBar: true,
+                  closeOnClick: false,
+                })
+              );
+            }
+          }}
+        >
+          Toggle error
+        </button>
+      </div>
     </div>
   );
 };
