afa41c11043ee119940cf2cb6b64be229bcbc5bc
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2022-02-14 07:04:11 -0800

    Write SideMenu tests

--- tests/20-SideMenu/SideMenu.test.tsx
+++ tests/20-SideMenu/SideMenu.test.tsx
@@ -2,6 +2,10 @@ import "@testing-library/jest-dom/extend-expect";
 
 import * as stories from "stories/20-SideMenu/20-SideMenu.stories";
 
+import { BasePinnedSectionProps, WithPinnedSectionProps } from "sidemenu/PinnedMenu";
+import { Boards, Icons } from "stories/20-SideMenu/01-PinnedMenu.stories";
+import { BoardsSectionProps, EmptySectionProps, LoadingSectionProps } from "sidemenu/BoardsMenuSection";
+import { Empty, Loading, Regular } from "stories/20-SideMenu/00-BoardsMenuSection.stories";
 import {
   Screen,
   fireEvent,
@@ -11,9 +15,12 @@ import {
   within,
 } from "@testing-library/react";
 
+import { CircleButtonProps } from "buttons/CircleButton";
+import { DropdownProps } from "common/DropdownListMenu";
 import React from "react";
 import { action } from "@storybook/addon-actions";
 import { composeStories } from "@storybook/testing-react";
+import exp from "constants";
 import { mocked } from "ts-jest/utils";
 import userEvent from "@testing-library/user-event";
 
@@ -24,37 +31,109 @@ const { SideMenuPreview } = composeStories(stories);
 test("Renders pinned menu", async () => {
   render(<SideMenuPreview />);
 
-  //TODO: fill this
+  const sections = document.getElementsByTagName("section");
+
+  const argsItems = ((Icons.args as WithPinnedSectionProps)
+  .items as (CircleButtonProps & { id: string, menuOptions?: DropdownProps["options"] })[]);
+  const linkArgs = argsItems.filter(item => !item.menuOptions);
+  const buttonArgs = argsItems.filter(item => item.menuOptions);
+
+  expect(within(sections[0]).getByLabelText((Icons!.args! as BasePinnedSectionProps).sectionId!)).toBeVisible();
+  expect(within(sections[0]).getAllByRole("link")).toHaveLength(linkArgs.length);
+  expect(within(sections[0]).getAllByRole("button")).toHaveLength(buttonArgs.length);
+
+  expect(within(sections[1]).getByLabelText((Boards!.args! as BasePinnedSectionProps).sectionId!)).toBeVisible();
+  expect(within(sections[1]).getAllByRole("link")).toHaveLength((Boards.args as WithPinnedSectionProps).items.length);
 });
 
 test("Pinned Menu doesn't render when turned off", async () => {
-  render(<SideMenuPreview />);
+  render(<SideMenuPreview showPinned={false} />);
 
-  //TODO: fill this
+  expect(screen.getByLabelText((Icons!.args! as BasePinnedSectionProps).sectionId!)).not.toBeVisible();
+  expect(screen.getByLabelText((Boards!.args! as BasePinnedSectionProps).sectionId!)).not.toBeVisible();
 });
 
 test("Renders boards menu", async () => {
   render(<SideMenuPreview />);
 
-  //TODO: fill this
+  const sections = document.getElementsByTagName("section");
+
+  expect(within(sections[2]).getByText(Regular!.args!.title!)).toBeVisible();
+  expect(within(sections[2]).getAllByRole("link")).toHaveLength((Regular.args as BoardsSectionProps).boards!.length);
+
+  expect(within(sections[3]).getByText(Loading!.args!.title!)).toBeVisible();
+  expect(within(sections[3]).getAllByLabelText("loading board placeholder")).toHaveLength((Loading.args as LoadingSectionProps).placeholdersCount);
+
+  expect(within(sections[4]).getByText(Empty!.args!.title!)).toBeVisible();
+  expect(within(sections[4]).queryAllByRole("link")).toHaveLength(0);
+  expect(within(sections[4]).getByText((Empty.args as EmptySectionProps).emptyTitle)).toBeVisible();
+  expect(within(sections[4]).getByText((Empty.args as EmptySectionProps).emptyDescription)).toBeVisible();
+
+  //TODO: Replace above with the following once updated story merged
+  // expect(within(sections[2]).getByText("Recent boards")).toBeVisible();
+  // expect(within(sections[2]).getAllByRole("link")).toHaveLength(4);
+
+  // expect(within(sections[3]).getByText("Fandom boards")).toBeVisible();
+  // expect(within(sections[3]).getAllByRole("link")).toHaveLength(3);
+
+  // expect(within(sections[4]).getByText(Regular!.args!.title!)).toBeVisible();
+  // expect(within(sections[4]).getAllByRole("link")).toHaveLength((Regular.args as BoardsSectionProps).boards!.length);
+
+  // expect(within(sections[5]).getByText(Loading!.args!.title!)).toBeVisible();
+  // expect(within(sections[5]).getAllByLabelText("loading board placeholder")).toHaveLength((Loading.args as LoadingSectionProps).placeholdersCount);
+
+  // expect(within(sections[6]).getByText(Empty!.args!.title!)).toBeVisible();
+  // expect(within(sections[6]).queryAllByRole("link")).toHaveLength(0);
+  // expect(within(sections[6]).getByText((Empty.args as EmptySectionProps).emptyTitle)).toBeVisible();
+  // expect(within(sections[6]).getByText((Empty.args as EmptySectionProps).emptyDescription)).toBeVisible();
 });
 
-//Currently not rendered in Storybook. Need to add menuOptions arg in SideMenu story before testing
 test("Renders boards dropdown", async () => {
   render(<SideMenuPreview />);
 
-  //TODO: fill this
+  const actionReturn = jest.fn();
+  mocked(action).mockReturnValue(actionReturn);
+  
+  userEvent.click(screen.getByLabelText("board menu options"));
+  await waitFor(() => {
+    expect(screen.getByText("Dismiss notifications")).toBeVisible();
+  });
+  userEvent.click(screen.getByText("Dismiss notifications"));    
+  await waitFor(() => {
+    expect(action).toBeCalledWith("dismissNotifications");
+  });
+
+  await waitFor(() => {
+    expect(screen.queryByText("Dismiss notifications")).not.toBeInTheDocument();
+  });
 });
 
 test("Renders board filter", async () => {
   render(<SideMenuPreview />);
 
-  //TODO: fill this
+  expect(screen.getByRole("searchbox")).toHaveAccessibleName("board filter");
+  expect(screen.getByRole("searchbox")).toEqual(screen.getByPlaceholderText("Filter boards"));
 });
 
 test("Correctly propagates filter change on text entry", async () => {
   render(<SideMenuPreview />);
 
-  //TODO: fill this
+  const actionReturn = jest.fn();
+  mocked(action).mockReturnValue(actionReturn);
+
+  const boardFilter = screen.getByRole("searchbox");
+  userEvent.type(boardFilter, "meta");
+  await waitFor(() => {
+    expect(action).toBeCalledWith("filterBoards");
+    expect(actionReturn).toBeCalledWith("meta");
+  });
+  expect(boardFilter).toHaveValue("meta");
+
+  userEvent.type(boardFilter, "{backspace}{backspace}");
+  await waitFor(() => {
+    expect(action).toBeCalledWith("filterBoards");
+    expect(actionReturn).toBeCalledWith("me");
+  });
+  expect(boardFilter).toHaveValue("me");
 });
 
