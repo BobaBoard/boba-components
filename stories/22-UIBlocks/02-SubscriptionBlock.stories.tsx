47cf8e8575b53cf4a23c3decc8d96bce787d2a9f
Essential Randomness <essential.randomn3ss@gmail.com>
2022-06-08 17:18:01 -0700

    create subscription block

---
+++ stories/22-UIBlocks/02-SubscriptionBlock.stories.tsx
@@ -0,0 +1,43 @@
+import {
+  EDITOR_TEXT_VALUES,
+  getInitialTextString,
+} from "../utils/editor-controls";
+import SubscriptionBlock, {
+  SubscriptionBlockProps,
+} from "../../src/blocks/SubscriptionBlock";
+
+import React from "react";
+import { Story } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import crown from "../images/crown.png";
+import tuxedoAvatar from "../images/tuxedo-mask.jpg";
+
+export default {
+  title: "UI Blocks/Subscription Block",
+  component: SubscriptionBlock,
+};
+
+const Template: Story<SubscriptionBlockProps> = (args) => (
+  <SubscriptionBlock {...args} />
+);
+
+export const Single = Template.bind({});
+Single.args = {
+  title: "The latest from BoobaBoard",
+  lastUpdatedTime: "4/20/69",
+  lastUpdatedTimeLink: {
+    onClick: action("withHref"),
+    href: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+  showOlderLink: {
+    onClick: action("withHref"),
+    href: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+  post: getInitialTextString(EDITOR_TEXT_VALUES.WITH_IMAGE),
+  secretIdentity: {
+    name: "Tuxedo Mask",
+    avatar: `/${tuxedoAvatar}`,
+    accessory: crown,
+    color: "#f30cb5",
+  },
+};
