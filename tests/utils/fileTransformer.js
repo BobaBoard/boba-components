1a8d9fe8265b2e91cabca5c82e63edd119f3eb3a
Essential Randomness <essential.randomn3ss@gmail.com>
2022-06-19 20:32:37 -0700

    get webpack to compile again

--- tests/utils/fileTransformer.js
+++ tests/utils/fileTransformer.js
@@ -1,6 +1,8 @@
 import path from "path";
 export default {
   process(_, filename) {
-    return "module.exports = " + JSON.stringify(path.basename(filename)) + ";";
+    return {
+      code: "module.exports = " + JSON.stringify(path.basename(filename)) + ";",
+    };
   },
 };
