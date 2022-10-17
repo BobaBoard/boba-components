4e3d40ad5c40669abe3e1783ec3f6138ea0113a0
Essential Randomness <essential.randomn3ss@gmail.com>
2022-10-16 19:41:47 -0700

    add .html to jest file transformer

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ export default {
   testEnvironment: "jsdom",
   transform: {
     "\\.(js|ts|tsx)$": "babel-jest",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|html)$":
       "<rootDir>/tests/utils/fileTransformer.js",
   },
   moduleNameMapper: {
