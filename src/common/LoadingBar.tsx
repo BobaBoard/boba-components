5122251ef7c04a6f412b03002ad44aac2f90ef08
Essential Randomness <essential.randomn3ss@gmail.com>
2022-11-07 18:40:31 -0800

    remove useless isomorphic layout effect on loading bar

--- src/common/LoadingBar.tsx
+++ src/common/LoadingBar.tsx
@@ -1,7 +1,6 @@
 import Color from "color";
 import React from "react";
 import classnames from "classnames";
-import useLayoutEffect from "use-isomorphic-layout-effect";
 
 const HEIGHT = 1;
 const INCREASE_INTERVAL = 700;
@@ -20,7 +19,7 @@ const LoadingBar = (props: LoadingBarProps) => {
   const progressTimeout = React.useRef<NodeJS.Timeout>();
   const barRef = React.useRef<HTMLDivElement>(null);
 
-  useLayoutEffect(() => {
+  React.useEffect(() => {
     if (!props.loading) {
       if (progressTimeout.current && barRef.current) {
         clearTimeout(progressTimeout.current);
