836dee0a82d50c81d626662320f709c78e957c2e
Sel <dimensionstraveler@gmail.com>
2023-07-03 16:31:31 -0400

    Modification of hidden thread preview to work with callback; wrote simple test story

--- src/thread/ThreadPreview.tsx
+++ src/thread/ThreadPreview.tsx
@@ -1,37 +1,22 @@
-import { Post, PostHandler, TagType, TagsType } from "@bobaboard/ui-components";
-import { PostData, ThreadSummaryType } from "types/Types";
-import { PostOptions, usePostOptions } from "components/options/usePostOptions";
-import {
-  addThreadHandlerRef,
-  removeThreadHandlerRef,
-} from "utils/scroll-utils";
-
 import React from "react";
-import { THREAD_VIEW_OPTIONS } from "components/core/editors/utils";
-import { faFilter } from "@fortawesome/free-solid-svg-icons";
-import { formatDistanceToNow } from "date-fns";
-import noop from "noop-ts";
-import { useCachedLinks } from "../../hooks/useCachedLinks";
-import { useCurrentRealmBoardId } from "contexts/RealmContext";
-import { useForceHideIdentity } from "../../hooks/useForceHideIdentity";
-import { useSetThreadHidden } from "lib/api/hooks/thread";
-import { withEditors } from "components/core/editors/withEditors";
 
 
-const HiddenThread: React.FC<{
-  thread: ThreadSummaryType;
-}> = ({ thread }) => {
-  const setThreadHidden = useSetThreadHidden();
+export const HiddenThread: React.FC<HiddenThreadProps> = ({ 
+    threadId, 
+    boardId, 
+    threadHidden, 
+    setHideCallback 
+}) => {
   return (
-    <div className="post hidden" key={thread.id}>
+    <div className="post hidden" key={threadId}>
       This thread was hidden{" "}
       <a
         href="#"
         onClick={(e) => {
-          setThreadHidden({
-            threadId: thread.id,
-            boardId: thread.parentBoardId,
-            hide: !thread.hidden,
+          setHideCallback({
+            threadId: threadId,
+            boardId: boardId,
+            hide: !threadHidden,
           });
           e.preventDefault();
         }}
@@ -52,3 +37,15 @@ const HiddenThread: React.FC<{
     </div>
   );
 };
+
+export interface HiddenThreadProps {
+  threadId: string;
+  boardId: string;
+  threadHidden: boolean;
+  setHideCallback: ({ threadId,
+                      boardId,
+                      hide, } : 
+                    { threadId: string, 
+                      boardId: string, 
+                      hide: boolean }) => void;
+}
---
+++ stories/Thread/102-ThreadPreview.stories.tsx
@@ -0,0 +1,36 @@
+import { HiddenThread, HiddenThreadProps } from "thread/ThreadPreview";
+
+import type { Meta, Story/*, StoryObj*/ } from "@storybook/react";
+
+export default {
+  title: "ThreadPreview / Hidden Thread",
+  component: HiddenThread,
+};
+
+const mockUnhide =  ({ 
+  threadId,
+  boardId,
+  hide, 
+} : { 
+  threadId: string, 
+  boardId: string, 
+  hide: boolean 
+}) => {
+  if(hide) {
+    console.log("hiding thread %s on board %s", threadId, boardId);
+  } else {
+    console.log("unhiding thread %s on board %s", threadId, boardId);
+  }
+};
+
+const ThreadPreviewTemplate: Story<HiddenThreadProps> = (args) => {
+  return <HiddenThread {...args} />;
+};
+
+export const Default = ThreadPreviewTemplate.bind({});
+Default.args = {
+  threadId: '0',
+  boardId: '0',
+  threadHidden: true,
+  setHideCallback: mockUnhide,
+};
