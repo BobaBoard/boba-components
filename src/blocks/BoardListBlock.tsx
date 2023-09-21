ce9346692b62c3b7c98c67831f78bfbc3cd80646
ria <42520601+riazaia@users.noreply.github.com>
2023-09-21 19:30:06 -0300

    Allow a board to be selected ft. ms boba

--- src/blocks/BoardListBlock.tsx
+++ src/blocks/BoardListBlock.tsx
@@ -1,72 +1,60 @@
 import React from "react";
 import { extractCompounds } from "utils/compound-utils";
 
-// Create all the subcomponents
-const Empty = (props: EmptyProps) => <p>{props.placeholder}</p>;
-const Item = (props: ItemProps) => {
-  const handleClick = () => {
-    window.alert(`${props.slug}`);
-    props.setSelectedBoardSlug(`${props.slug}`);
-  };
-  return <p onClick={handleClick}>{props.slug}</p>;
-};
-const Details = (props: DetailsProps) => (
+const Empty: React.FC<unknown> = ({ children }) => <p>{children}</p>;
+
+const Item = ({ slug, description, _selected, _onSelected }: ItemProps) => (
   <>
-    <p>{props.selectedBoardSlug}</p>
-    <p>{props.description}</p>
+    <p onClick={() => _onSelected?.(slug)}>!{slug}</p>
+    {_selected && <p>{description}</p>}
   </>
 );
 
-// Create the Layout components
 const BoardListBlock: BoardListBlockCompound = (props: BoardListBlockProps) => {
   const items = extractCompounds(props.children, Item);
-  const details = extractCompounds(props.children, Details);
-
-  const [selectedBoardSlug, setSelectedBoardSlug] = React.useState("None");
+  const empty = extractCompounds(props.children, Empty);
 
   return (
     <>
       <h2>{props.title}</h2>
-      {!items.length && <BoardListBlock.Empty placeholder="tis nothing here" />}
-      {items}
-      {details}
+      {items.length === 0 && empty}
+      {items.map((item) => {
+        const ItemWithFunction = React.cloneElement(item, {
+          ...item.props,
+          _selected: item.props.slug === props.selectedBoardSlug,
+          _onSelected: () => props.onSelectBoard(item.props.slug),
+        });
+
+        return ItemWithFunction;
+      })}
     </>
   );
 };
 
 type BoardListBlockCompound = React.FC<BoardListBlockProps> & {
-  Empty: React.FC<EmptyProps>;
+  Empty: React.FC<unknown>;
   Item: React.FC<ItemProps>;
-  Details: React.FC<DetailsProps>;
 };
 
 export interface EmptyProps {
-  placeholder: string;
+  children: React.ReactNode;
 }
-
 export interface ItemProps {
   slug: string;
-  selectedBoardSlug: string;
-  setSelectedBoardSlug: React.Dispatch<React.SetStateAction<string>>;
-}
-
-export interface DetailsProps {
-  slug: string;
-  description: string;
-  selectedBoardSlug: string;
+  description?: string;
+  _onSelected?: (slug: string) => void;
+  _selected?: boolean;
 }
 
 export interface BoardListBlockProps {
-  icon: string;
-  title: string;
-  selectedBoardSlug: string;
-  children: React.ReactNode[];
+  icon?: string;
+  title?: string;
+  selectedBoardSlug?: string;
+  onSelectBoard: (slug: string) => void;
+  children?: React.ReactNode;
 }
 
-// Add the subcomponents as the same-named property of the
-// compound.
 BoardListBlock.Empty = Empty;
 BoardListBlock.Item = Item;
-BoardListBlock.Details = Details;
 
 export default BoardListBlock;
